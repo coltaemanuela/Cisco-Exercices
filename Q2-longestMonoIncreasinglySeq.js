695478c66cbf322fb21f4e725ebab771e3c72140
Emma <emanuela.colta@mobiversal.com>
2017-11-13 01:11:21 +0000

    answered questions 1, 2 and 3

---
+++ Q2-longestMonoIncreasinglySeq.js
@@ -0,0 +1,36 @@
+var bigArray = [100,4,2,3,5,7,9,11,8,10,30,32,34,20,18,17,15,22,24,25,26,15];
+var subArr = []; 
+var reversedSubArr = [];
+
+subArr[0] = bigArray[0]; //my subArr will, initially, contain the first element from bigArr
+var j = 0;
+
+for(var i=0; i<= bigArray.length-1; ++i)
+{
+    if ( subArr[0]  > bigArray[i]  ) subArr[0] = bigArray[i]; // if at some point while iterating the bigArray, I can find an element lower than subArr[0], reset the starting value of the subArr to it
+    if ( bigArray[i] > subArr[j] ) {                          // if there is a number bigger than the maximum found until this point, push it.
+        subArr.push( bigArray[i] );
+        j++;
+    }
+    var c = find(subArr, 0, j, bigArray[i]); // perform search operation in subArr 
+    subArr[c] = bigArray[i];                 // and after getting the result,start all over again
+}
+
+console.log("longest subsequence:  ",subArr);
+
+                                            //reverse the longest found subsequence
+for(var j = subArr.length -1; j >= 0; --j){  // Iterate the array containing the subsequence starting from end to beginning
+    reversedSubArr.push(subArr[j]);          // and push all elements in the empty array 
+}
+console.log("reversed subsequence: ", reversedSubArr);
+
+//recursive function
+function find( arr, left, right,  x)
+{
+    if(left === right) return left;                                // if array contains only one element, return it
+    if(left > right) return false;                                 // if the element in the left > the one in the right, stop here.      
+    var mid = (left+right)/2;                                      // find the middle point of the array 
+    if( arr[mid] < x && arr[mid+1] > x )  ++mid;                   // find in which half is situated the x, If it is bigger than the middle value, go to the right
+    else if( arr[mid] > x ) return find(arr, left, mid, x);        // else,go to the left and keep searching in the first half of the array
+    return find(arr, mid+1, right, x);                             // keep searching in the second half of the array
+}
---
+++ Q3-SumArrays.js
@@ -0,0 +1,21 @@
+function result (array1, array2, givenNumber) {
+    var map2 = {};  
+    /* map2 is gonna be a map between the value of the elements from the second array and their indexes:            
+       e.g: for the array  [-2, 0, 3, 4, 7, 8, 9, 23, 24], 
+                    map2 will be: "-2": 0,  "0": 1,  ...,"24": 8  */            
+     
+    //  iterate the second array just once and, then, using this map2, check if the other term exists
+    for (var j = 0; j < array2.length; ++j) {        
+        map2[array2[j]] = j;// Build the map with the general format <value>: <index>
+    }    
+   
+    for (var i = 0; i < array1.length; ++i) {  // Iterate the first array
+        var b = givenNumber - array1[i];        // `b` is the number I need.                                 
+        if (map2[b]) {                          // If it exists in `map2`, I found the solution
+            console.log('The given number, '+ givenNumber+', is the sum of the elements identified by the indices ',i +' , respectively '+ map2[b]);
+            return [i, map2[b]];
+        }
+    }
+}
+//call the function result() with its 3 parameters: array1, array2 and a given number. Here, I will hardcode them for the purpose pf testing
+result( [0, 1, 2, 3, 4, 7, 8, 9,  23, 24, 42], [-2, 0, 3, 4, 7, 8, 9, 23, 24],   14);
